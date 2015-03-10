4f29b96e6fe3afb04cb1de03495289713bc0ee34
Jeffrey Wan <Jwan622@gmail.com>
2015-03-10 14:59:17 -0600

    fixed views and presenter

--- app/presenters/trips_presenter.rb
+++ app/presenters/trips_presenter.rb
@@ -5,7 +5,7 @@ class TripsPresenter
   end
 
   def trips
-    if data[:search_type] != "all_projects"
+    if data[:search_type]
       Trip.send("by_#{@data[:search_type]}", @data[:search_value])
     else
       Trip.all_by_ranking
