6a6963270064ce0c8e67453705c46e502ceb2735
Jeffrey Wan <Jwan622@gmail.com>
2015-03-25 13:44:51 -0600

    fixed small bug with account activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,12 +3,14 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      #the !user.activated this is so someone who gets a hold of the email can't just click on the link and get logged in. It's only good once
+      # and then afterwards the user needs to login with the username and password.
       user.update_attribute(:activated, true)
       log_in user
       flash[:success] = "Account activated!"
       redirect_to root_url
     else
-      flash[:danger] = "Invalid activation link"
+      flash[:error] = "Invalid activation link"
       redirect_to root_url
     end
   end
