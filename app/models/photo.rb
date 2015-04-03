0b512fb3079d1045fc19e49800fc844404245fe8
Jeffrey Wan <Jwan622@gmail.com>
2015-04-02 21:24:31 -0600

    added imagemagick and it works locally

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,10 +4,13 @@ class Photo < ActiveRecord::Base
   belongs_to :trip_info
 
   has_attached_file :avatar,
-                    :styles => { :medium => "300x400>",
-                                 :thumb => "100x100>",
-                                 :city => "280x340",
-                                 :large => "500x500"},
+                    :styles => { :medium => "300x400#>",
+                                 :thumb => "100x100#>",
+                                 :city => "260x250#",
+                                 :large => "500x500#"},
+                    # :convert_options => { :medium => "-quality 70 -interlace Plane -strip",
+                    #                       :small => "-quality 70 -interlace Plane -strip",
+                    #                       :thumb => "-quality 70 -interlace Plane -strip" },
                     :default_url => "/images/:style/logo3.png"
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
