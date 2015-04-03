a4efc01290e1b60e30e6691eab92864497b3e059
Jeffrey Wan <Jwan622@gmail.com>
2015-04-02 21:47:01 -0600

    edited images with imagemagick

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,13 +4,10 @@ class Photo < ActiveRecord::Base
   belongs_to :trip_info
 
   has_attached_file :avatar,
-                    :styles => { :medium => "300x400#>",
+                    :styles => { :medium => "310x300#>",
                                  :thumb => "100x100#>",
                                  :city => "260x250#",
-                                 :large => "500x500#"},
-                    # :convert_options => { :medium => "-quality 70 -interlace Plane -strip",
-                    #                       :small => "-quality 70 -interlace Plane -strip",
-                    #                       :thumb => "-quality 70 -interlace Plane -strip" },
+                                 :large => "500x300#"},
                     :default_url => "/images/:style/logo3.png"
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
